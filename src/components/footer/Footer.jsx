911a20ec85a92562065331fff31808c543c043fa
iamsr5675 <saurabhranjan5675@gmail.com>
2021-12-07 03:16:34 +0530

    Added footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="text">
+        Copyright © 2020, Bookstore Private Limited. All Rights Reserved
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 
 function Home() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
