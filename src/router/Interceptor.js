d4db34d8badbb3966ec4cdab010b94828b03ba83
iamsr5675 <saurabhranjan5675@gmail.com>
2021-12-21 15:38:59 +0530

    Added interceptor

---
+++ src/router/Interceptor.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+axios.interceptors.request.use(function (config) {
+  if (config.url.includes("cart")) {
+    config.headers.token = localStorage.getItem("id");
+  }
+
+  if (config.url.includes("wish")) {
+    config.headers.token = localStorage.getItem("id");
+  }
+
+  if (config.url.includes("edit_user")) {
+    config.headers.token = localStorage.getItem("id");
+  }
+
+  console.log(config);
+  return config;
+});
+
+axios.interceptors.response.use(function (response) {
+  console.log(response);
+  return response;
+});
--- src/service/DataService.jsx
+++ src/service/DataService.jsx
@@ -8,8 +8,7 @@ let configObjGetBook = {
 
 export const book = async () => {
   let response = await axios.get(
-    "https://new-bookstore-backend.herokuapp.com/bookstore_user/get/book",
-    configObjGetBook
+    "https://new-bookstore-backend.herokuapp.com/bookstore_user/get/book"
   );
   return response;
 };
@@ -18,16 +17,14 @@ export const addCartItems = async (obj) => {
   console.log(obj);
   let response = await axios.post(
     `https://new-bookstore-backend.herokuapp.com/bookstore_user/add_cart_item/${obj}`,
-    obj,
-    configObjGetBook
+    obj
   );
   return response;
 };
 
 export const getCartItems = async () => {
   let response = await axios.get(
-    "https://new-bookstore-backend.herokuapp.com/bookstore_user/get_cart_items",
-    configObjGetBook
+    "https://new-bookstore-backend.herokuapp.com/bookstore_user/get_cart_items"
   );
   return response;
 };
@@ -37,16 +34,14 @@ export const cartItemQuantity = async (obj, data) => {
   console.log(data);
   let response = await axios.put(
     `https://new-bookstore-backend.herokuapp.com/bookstore_user/cart_item_quantity/${obj}`,
-    data,
-    configObjGetBook
+    data
   );
   return response;
 };
 
 export const DeleteBooks = async (obj) => {
   let response = await axios.delete(
-    `https://new-bookstore-backend.herokuapp.com/bookstore_user/remove_cart_item/${obj}`,
-    configObjGetBook
+    `https://new-bookstore-backend.herokuapp.com/bookstore_user/remove_cart_item/${obj}`
   );
   return response;
 };
@@ -54,8 +49,7 @@ export const DeleteBooks = async (obj) => {
 export const CustomerDetailsEdit = async (obj) => {
   let response = await axios.put(
     "https://new-bookstore-backend.herokuapp.com/bookstore_user/edit_user",
-    obj,
-    configObjGetBook
+    obj
   );
   return response;
 };
@@ -64,24 +58,21 @@ export const addWishlistItems = async (obj) => {
   console.log(obj);
   let response = await axios.post(
     `https://new-bookstore-backend.herokuapp.com/bookstore_user/add_wish_list/${obj}`,
-    obj,
-    configObjGetBook
+    obj
   );
   return response;
 };
 
 export const getWishlistItems = async (obj) => {
   let response = await axios.get(
-    "https://new-bookstore-backend.herokuapp.com/bookstore_user/get_wishlist_items",
-    configObjGetBook
+    "https://new-bookstore-backend.herokuapp.com/bookstore_user/get_wishlist_items"
   );
   return response;
 };
 
 export const deleteWishlistItems = async (obj) => {
   let response = await axios.delete(
-    `https://new-bookstore-backend.herokuapp.com/bookstore_user/remove_cart_item/${obj}`,
-    configObjGetBook
+    `https://new-bookstore-backend.herokuapp.com/bookstore_user/remove_cart_item/${obj}`
   );
   return response;
 };
